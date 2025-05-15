7b9a14ec6ebf13d3636f1254502f2b9583c8ac13
rogeriosaraceni <rsaraceni@gmail.com>
2025-05-15 08:34:57 -0300

    config index dos slides

--- js/slide.js
+++ js/slide.js
@@ -48,9 +48,6 @@ export default class Slide{
         this.dist.finalPosition = this.dist.movePosition;
     }
 
-
-
-
     //adiciona cada evento do slide
     addSlideEvents() {
         this.wrapper.addEventListener('mousedown', this.onStart);
@@ -59,7 +56,6 @@ export default class Slide{
         this.wrapper.addEventListener('touchend', this.onEnd);
     }
 
-
     //vincular o .this
     bindEvents() {
         this.onStart = this.onStart.bind(this);
@@ -67,10 +63,43 @@ export default class Slide{
         this.onEnd = this.onEnd.bind(this);
     }
 
+    //Slides config
+    slidePosition(slide) {
+        const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+        return -(slide.offsetLeft - margin)
+    }
+
+    slidesConfig() {
+        this.slideArray = [...this.slide.children].map(element => {
+            const position = this.slidePosition(element);
+            return {
+                position,
+                element
+            }
+        })
+    }
+
+    slideIndexNav(index) { 
+        const last = this.slideArray.length - 1;
+        this.index = {
+            prev: index ? index - 1 : undefined,
+            active: index,
+            next: index === last ? undefined : index + 1
+        }
+    }
+
+    changeSlide(index) {
+        const active = this.slideArray[index];
+        this.moveSlide(active.position);
+        this.slideIndexNav(index)
+        this.dist.finalPosition = active.position;
+    }
+
     //para iniciar os eventos
     init() {
         this.bindEvents();
         this.addSlideEvents();
+        this.slidesConfig()
         return this;
     }
 }
